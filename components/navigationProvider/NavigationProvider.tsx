ae3684d6827f04a1f64536144620e1528bb18574
LukasKubec <devian3@gmail.com>
2023-02-25 23:07:46 +0100

    Components / NavigationProvider

---
+++ components/navigationProvider/NavigationProvider.tsx
@@ -0,0 +1,31 @@
+import { navigationContext } from "@/lib";
+import { ReactNode, useState } from "react";
+
+interface NavigationProvider {
+  children: ReactNode;
+}
+
+const NavigationContextProvider = navigationContext.Provider;
+
+export const NavigationProvider = ({
+  children,
+}: NavigationProvider): JSX.Element => {
+  const [header, setHeader] = useState<string | undefined>(undefined);
+  const [navigationIsOpen, setNavigationIsOpen] = useState(false);
+  const handleClose = () => setNavigationIsOpen(false);
+  const handleOpen = () => setNavigationIsOpen(true);
+
+  return (
+    <NavigationContextProvider
+      value={{
+        header,
+        setHeader,
+        navigationIsOpen,
+        closeNavigation: handleClose,
+        openNavigation: handleOpen,
+      }}
+    >
+      {children}
+    </NavigationContextProvider>
+  );
+};

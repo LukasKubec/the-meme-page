ee09d9e9530d5e16490bc56e26cccb9cbe5912d4
LukasKubec <devian3@gmail.com>
2023-02-25 23:12:08 +0100

    Components / Navigation / Navigation component with Navigation Context

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,60 +1,50 @@
 import {
   AppBar,
-  Box,
-  Drawer,
   Grid,
+  styled,
   Toolbar,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { useState } from "react";
-import { MenuDivider } from "./MenuDivider";
-import { CloseButton } from "./CloseButton";
 import { OpenMenuButton } from "./OpenMenuButton";
+import { useNavigationContext } from "@/lib";
+
+const NavigationAppBar = styled(AppBar)(({ theme }) => ({
+  marginBottom: theme.spacing(1.5),
+  boxShadow: "none",
+}));
 
 const Navigation = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { header, openNavigation } = useNavigationContext();
   const theme = useTheme();
   const matchesMd = useMediaQuery(theme.breakpoints.down("md"));
 
-  const handleClose = () => setIsOpen(false);
-  const handleOpen = () => setIsOpen(true);
-
   return (
-    <AppBar position="static" color="transparent">
+    <NavigationAppBar position="static" color="transparent">
       <Toolbar>
-        <Grid container direction="row" alignItems="center">
-          <Grid item md={12} xs={11}>
-            <Typography variant="h1" component="h1" align={matchesMd ? "left" : "center"}>
-              Memes!
-            </Typography>
-          </Grid>
+        <Grid
+          container
+          direction={matchesMd ? "row-reverse" : "row"}
+          alignItems="center"
+        >
           {matchesMd && (
             <Grid item xs={1}>
-              <OpenMenuButton setIsOpen={handleOpen} />
+              <OpenMenuButton setIsOpen={openNavigation} />
             </Grid>
           )}
+          <Grid item md={12} xs={11}>
+            <Typography
+              variant="h1"
+              component="h1"
+              align={matchesMd ? "left" : "center"}
+            >
+              {header}
+            </Typography>
+          </Grid>
         </Grid>
       </Toolbar>
-
-      <Drawer
-        anchor="right"
-        variant={matchesMd ? "temporary" : "permanent"}
-        open={isOpen}
-        onClose={() => (isOpen ? handleClose() : undefined)}
-      >
-        <Box>
-          {matchesMd && (
-            <>
-              <CloseButton onClick={handleClose} />
-              <MenuDivider />
-            </>
-          )}
-          <Box></Box>
-        </Box>
-      </Drawer>
-    </AppBar>
+    </NavigationAppBar>
   );
 };
 

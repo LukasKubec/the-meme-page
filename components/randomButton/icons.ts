b67e8b409872a6e0b8355a78129ed7f94956de6d
LukasKubec <devian3@gmail.com>
2023-02-26 00:42:23 +0100

    Components / RandomButton - extract styled icons

---
+++ components/randomButton/icons.ts
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+import { KeyboardAltOutlined, SwipeOutlined } from "@mui/icons-material";
+
+export const RandomSwipeIcon = styled(SwipeOutlined)`
+  margin-left: 0.5rem;
+`;
+
+export const RandomKeyboardIcon = styled(KeyboardAltOutlined)`
+  margin-left: 0.5rem;
+`;

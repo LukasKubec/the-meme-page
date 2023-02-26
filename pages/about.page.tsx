083e36f4f834a3a9d96d7a1628da835beef9d6f6
LukasKubec <devian3@gmail.com>
2023-02-26 01:14:23 +0100

    Pages / about page

---
+++ pages/about.page.tsx
@@ -0,0 +1,19 @@
+import { useNavigationContext } from "@/lib";
+import { Typography } from "@mui/material";
+import Link from "next/link";
+import { OpenInNewOutlined } from "@mui/icons-material";
+
+const AboutPage = (): JSX.Element => {
+  useNavigationContext("About");
+  return (
+    <Typography variant="body1" align="center" margin={1}>
+      This website is my sandbox to play with NextJS. Check out{" "}
+      <Link href="https://github.com/LukasKubec/the-meme-page" target="_blank">
+        Github <OpenInNewOutlined />
+      </Link>
+      .
+    </Typography>
+  );
+};
+
+export default AboutPage;

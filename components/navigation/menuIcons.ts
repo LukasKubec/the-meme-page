7397e07b64833ee27a9af17d4d6a31558119e435
LukasKubec <devian3@gmail.com>
2023-02-26 01:14:54 +0100

    Components / menuIcons - add about icon

--- components/navigation/menuIcons.ts
+++ components/navigation/menuIcons.ts
@@ -1,10 +1,14 @@
 import { styled } from "@mui/material";
-import { CelebrationOutlined, PsychologyOutlined } from "@mui/icons-material";
+import { CelebrationOutlined, PsychologyOutlined, InfoOutlined } from "@mui/icons-material";
 
 export const MenuCelebrationOutlined = styled(CelebrationOutlined)(({ theme }) => ({
   color: theme.palette.primary.main,
 }));
 
 export const ChuckNorrisOutlined = styled(PsychologyOutlined)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+export const InfoOutlinedStyled = styled(InfoOutlined)(({ theme }) => ({
     color: theme.palette.primary.main,
 }));

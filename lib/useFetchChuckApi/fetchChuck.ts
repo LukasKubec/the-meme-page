930df8fd698bace7baa9bc3543c6e06dfb3e3c5b
LukasKubec <devian3@gmail.com>
2023-02-27 01:40:37 +0100

    lib / useFetchChuckApi - fetcher for useSwr

--- lib/useFetchChuckApi/fetchChuck.ts
+++ lib/useFetchChuckApi/fetchChuck.ts
@@ -1,16 +1,10 @@
 import { isChuckApiResponse } from "@/types";
 
-export const fetchChuck = async () => {
-  try {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    const json = await response.json();
-
-    if (!isChuckApiResponse(json)) {
-      throw new Error("Chuck API response is invalid");
-    }
-
-    return json;
-  } catch (error) {
-    throw error;
+export const fetcher = async (input: RequestInfo, init: RequestInit) => {
+  const res = await fetch(input, init);
+  const json = await res.json();
+  if(!isChuckApiResponse(json)) {
+    throw new Error('Not a ChuckApiResponse');
   }
+  return json;
 };

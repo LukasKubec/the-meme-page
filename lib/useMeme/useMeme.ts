47737dd34dd256ea3e00a341abb6a8b854e03914
Lukáš Kubec <lukas.kubec@mibcon.cz>
2024-07-21 11:06:55 +0200

    lib / useMeme - add "mounted" flag to prevent running setState on an unmounted component

--- lib/useMeme/useMeme.ts
+++ lib/useMeme/useMeme.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 type VisitedPredicate<T, V> = (item: T, visited: V[]) => boolean;
 type VisitedMapper<T, V> = (item: T) => V;
@@ -23,36 +23,42 @@ const useMeme = <T, V>({
   const [meme, setMeme] = useState<T | undefined>(undefined);
   const [loading, setLoading] = useState<boolean>(false);
   const [visited, setVisited] = useState<V[]>([]);
+  const isMounted = useRef(true);
 
   const setRandomMeme = async () => {
     if (
-      visitedPredicate &&
-      data.every((meme) => visitedPredicate(meme, visited))
+        visitedPredicate &&
+        data.every((meme) => visitedPredicate(meme, visited))
     ) {
       setVisited([]);
     }
 
     const randomMeme = data[Math.floor(Math.random() * data.length)];
     if (
-      randomMeme === meme ||
-      (visitedPredicate && visitedPredicate(randomMeme, visited))
+        randomMeme === meme ||
+        (visitedPredicate && visitedPredicate(randomMeme, visited))
     ) {
       setLoading(true);
       await setRandomMeme();
-      debugger;
       setLoading(false);
     } else {
       if (visitedMapper) {
         setVisited([...visited, visitedMapper(randomMeme)]);
       }
-      setMeme(randomMeme);
+      if (isMounted.current) {
+        setMeme(randomMeme);
+      }
     }
   };
 
   useEffect(() => {
+    isMounted.current = true;
     if (!meme) {
       void setRandomMeme();
     }
+    return () => {
+      isMounted.current = false;
+    };
   }, [meme]);
 
   return { meme, setRandomMeme, loading };

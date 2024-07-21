54051869cbc87d8cd6565030d1b2845a48bd7429
Lukáš Kubec <lukas.kubec@mibcon.cz>
2024-07-21 14:29:55 +0200

    Components / RandomButton - add fullWidth prop to display full width of parent

--- components/randomButton/RandomButton.tsx
+++ components/randomButton/RandomButton.tsx
@@ -7,13 +7,16 @@ import { RandomKeyboardIcon, RandomSwipeIcon } from "./icons";
 interface RandomButtonProps {
   onClick: () => void;
   label: string;
+  fullWidth?: boolean;
 }
 
-export const RandomButton = ({ onClick, label }: RandomButtonProps) => {
+export const RandomButton = ({ onClick, label, fullWidth }: RandomButtonProps) => {
   const theme = useTheme();
   const matchesSm = useMediaQuery(theme.breakpoints.down("sm"));
   return (
-    <Button variant="contained" size="large" onClick={onClick}>
+    <Button variant="contained" size="large" onClick={onClick} sx={{
+      width: fullWidth ? "100%" : "auto",
+    }}>
       {label}
       {matchesSm ? (
         <RandomSwipeIcon />

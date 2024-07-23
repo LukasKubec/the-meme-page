143985bd88f411b6d1bdb22a33a46952c6b7a24d
Lukáš Kubec <lukas.kubec@mibcon.cz>
2024-07-23 20:27:55 +0200

    Configuration / Next & Envs - types

---
+++ environment.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        IGNORE_BUILD_LINT?: "true";
+        PLAYWRIGHT_DEV?: "true" | "false";
+    }
+}
--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+/** @type {NodeJS.ProcessEnv} */
+const env = process.env;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx"],
   output: "standalone",
   swcMinify: true,
-  ignoreDuringBuilds: process.env.IGNORE_BUILD_LINT === "true",
+  ignoreDuringBuilds: env.IGNORE_BUILD_LINT === "true",
 };
 
 module.exports = nextConfig;
